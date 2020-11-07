9f3e9a0a66c3b7bd1bba611ff7ddb1bee82001b1
Suhail Manzoor <suhailski@gmail.com>
2020-11-07 16:00:15 +0400

    Display purses installed on the agoric wallet

---
+++ src/components/Sleeve.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="containers">
+    <div v-for="purse in purses" :key="purse.brandBoardId" class="card">
+      <div class="sleeve">
+        <div class="credit-card selected" style="background: #d0e1f9">
+          <div class="card-company">First Bank of Umq</div>
+          <div class="card-number">
+            <div class="digit-group">{{purse.brandBoardId}}</div>
+          </div>
+          <div class="card-expire">
+            <span class="card-text">Balance:</span>
+            {{purse.value}}
+            <span class="card-text">{{purse.brandPetname}}</span>
+          </div>
+          <div class="card-holder">John S. Smith</div>
+          <div class="card-type">{{purse.pursePetname}}</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "Sleeve",
+  computed: {
+    purses() {
+      return this.$store.getters["wallet/getPurses"];
+    },
+  },
+};
+</script>
+<style>
+</style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,28 @@
 <template>
-    <p>hello world</p>
+    <div id="activity-feed" class="view-wrap true-dom">
+    <div class="columns">
+      <!-- Left side column -->
+      <div class="column is-3"></div>
+
+      <!-- /Middle column -->
+      <div class="column is-6">
+        <Sleeve></Sleeve>
+      </div>
+
+      <!-- Right side column -->
+      <div class="column is-3">
+        
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import Sleeve from '../components/Sleeve.vue';
 export default {
   name: 'Home',  
+  components: {
+    Sleeve,
+  },
 };
 </script>
